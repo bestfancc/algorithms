11b1930d946720ad01e14383884fcc5bed4ec182
fanchengcheng <729940801@qq.com>
2018-09-24 23:48:31 +0800

    修改

--- main.cpp
+++ main.cpp
@@ -375,12 +375,13 @@ public:
 class BST
 {
 public:
+    //结点类
     class Node
     {
     public:
         double key;
         double val;
-        Node left,right;
+
         int N;
         Node() {
 
@@ -400,8 +401,11 @@ public:
             if (this->key == x.key && this->val == x.val && this->N == x.N) return false;
             return true;
         }
+        BST::Node *left;
+        BST::Node *right;
     };
     Node root;
+
     int size(Node x) {
         if(x.isNull()) {
             return 0;
@@ -413,17 +417,17 @@ public:
         return size(root);
     }
     double get(double key) {
-        return get(root,key);
+        return get(&root,key);
     }
-    double get(Node x, double key) {
+    double get(Node *x, double key) {
         //在以x为跟结点的子树中查找并返回key对应的值
         //如果找不到就返回NULL
-        if(x.isNull()) return NULL;
+        if((*x).isNull()) return 0.0;
         Comparable compareKey(key);
-        int cmp = compareKey.compareTo(x.key);
-        if(cmp < 0) return get(x.left,key);
-        else if(cmp > 0) return get(x.right,key);
-        else return x.val;
+        int cmp = compareKey.compareTo((*x).key);
+        if(cmp < 0) return get((*x).*left,key);
+        else if(cmp > 0) return get((*x).*right,key);
+        else return (*x).val;
     }
     void put(double key, double val) {
         //查找key，找到则更新它的值，否则为它创建一个新的结点
@@ -435,25 +439,25 @@ public:
         if(x.isNull()) return Node(key,val,1);
         Comparable compareKey(key);
         int cmp = compareKey.compareTo(x.key);
-        if(cmp < 0) x.left = put(x.left,key,val);
-        else if(cmp > 0) x.right = put(x.right,key,val);
+        if(cmp < 0) x.left = put(x.*left,key,val);
+        else if(cmp > 0) x.right = put(x.*right,key,val);
         else x.val = val;
-        x.N = size(x.left) + size(x.right) + 1;
+        x.N = size(x.*left) + size(x.*right) + 1;
         return x;
     }
     double min() {
         return min(root).key;
     }
     Node min(Node x) {
-        if(x.left.isNull()) return x;
-        return min(x.left);
+        if((x.*left).isNull()) return x;
+        return min(x.*left);
     }
     double max() {
         return max(root).key;
     }
     Node max(Node x) {
-        if(x.right.isNull()) return x;
-        return max(x.left);
+        if((x.*right).isNull()) return x;
+        return max(x.*left);
     }
     double floor(double key) {
         Node x = floor(root,key);
@@ -465,8 +469,8 @@ public:
         Comparable compareKey(key);
         int cmp = compareKey.compareTo(x.key);
         if(cmp == 0) return NULL;
-        if(cmp < 0) return floor(x.left,key);
-        Node t = floor(x.right,key);
+        if(cmp < 0) return floor(x.*left,key);
+        Node t = floor(x.*right,key);
         if(!t.isNull()) return t;
         else return x;
     }
@@ -476,9 +480,9 @@ public:
     Node select(Node x,int k) {
         //返回排名为k的结点
         if(x.isNull()) return NULL;
-        int t = size(x.left);
-        if(t > k) return select(x.left,k);
-        else if(t > k) return select(x.right, k-t-1);
+        int t = size(x.*left);
+        if(t > k) return select(x.*left,k);
+        else if(t > k) return select(x.*right, k-t-1);
         else return x;
     }
     int rank(double key) {
@@ -489,17 +493,17 @@ public:
         if(x.isNull()) return 0;
         Comparable compareKey(key);
         int cmp = compareKey.compareTo(x.key);
-        if(cmp < 0) return rank(key, x.left);
-        else if(cmp > 0) return 1 + size(x.left) + rank(key, x.right);
-        else return size(x.left);
+        if(cmp < 0) return rank(key, x.*left);
+        else if(cmp > 0) return 1 + size(x.*left) + rank(key, x.*right);
+        else return size(x.*left);
     }
     void deleteMin() {
         root = deleteMin(root);
     }
     Node deleteMin(Node x) {
-        if(x.left.isNull()) return x.right;
-        x.left = deleteMin(x.left);
-        x.N = size(x.left) + size(x.right) + 1;
+        if((x.*left).isNull()) return x.*right;
+        x.left = deleteMin(x.*left);
+        x.N = size(x.*left) + size(x.*right) + 1;
         return x;
     }
     void deleteOne(double key) {
@@ -509,31 +513,31 @@ public:
         if(x.isNull()) return NULL;
         Comparable compareKey(key);
         int cmp = compareKey.compareTo(x.key);
-        if(cmp < 0) x.left = deleteOne(x.left, key);
-        else if(cmp < 0) x.right = deleteOne(x.right, key);
+        if(cmp < 0) x.left = deleteOne(x.*left, key);
+        else if(cmp < 0) x.right = deleteOne(x.*right, key);
         else{
-            if(x.right.isNull()) return x.left;
-            if(x.left.isNull()) return x.right;
+            if((x.*right).isNull()) return x.*left;
+            if((x.*left).isNull()) return x.*right;
             Node t = x;
-            x = min(x.right);
-            x.right = deleteMin(t.right);
+            x = min(x.*right);
+            x.right = deleteMin(t.*right);
             x.left = t.left;
         }
-        x.N = size(x.left) + size(x.right) + 1;
+        x.N = size(x.*left) + size(x.*right) + 1;
         return x;
     }
     //打印所有键
     void print(Node x) {
         if(x.isNull()) return;
-        print(x.left);
+        print(x.*left);
         std::cout<<x.key<<endl;
-        print(x.right);
+        print(x.*right);
     }
     //范围查找操作
-    queue keys() {
+    queue <double > keys() {
         return keys(min(), max());
     }
-    queue keys(double lo, double hi) {
+    queue <double > keys(double lo, double hi) {
         queue< double > queue;
         keys(root, queue, lo, hi);
         return queue;
@@ -544,9 +548,9 @@ public:
         int cmplo = comparableLo.compareTo(x.key);
         Comparable comparableHi(hi);
         int cmphi = comparableHi.compareTo(x.key);
-        if(cmplo < 0) keys(x.left, queue, lo, hi);
+        if(cmplo < 0) keys(x.*left, queue, lo, hi);
         if(cmplo <=0 && cmphi >= 0) queue.push(x.key);
-        if(cmphi > 0) keys(x.right, queue, lo, hi);
+        if(cmphi > 0) keys(x.*right, queue, lo, hi);
     }
 
 
@@ -575,6 +579,15 @@ int main() {
     //堆排序
 //    HeapSort::main();
     //查找
+    BST bst;
+    bst.put(3.1, 3);
+    bst.put(2.1, 5);
+    bst.put(4.1, 2);
+    bst.put(6.1, 4);
+    bst.put(7.1, 9);
+    bst.put(6.1, 4);
+    bst.put(9.1, 6);
+    bst.print(bst.root);
 
 
     return 0;
