b312facdb0cb709b7d210649d92479f7ec7accff
bestfancc <bestfancc@gmail.com>
2018-12-03 23:26:48 +0800

    完成部分红黑树的方法

--- RedBlackBST.cpp
+++ RedBlackBST.cpp
@@ -10,3 +10,40 @@
 using namespace std;
 
 //红黑树
+RedBlackBST::Node::Node() {}
+RedBlackBST::Node::Node(double key, double val, int N, bool color) {
+    this->key = key;
+    this->val = val;
+    this->N = N;
+    this->color = color;
+}
+bool RedBlackBST::Node::operator==(RedBlackBST::Node x) {
+    if (this->key == x.key && this->val == x.val && this->N == x.N && this->color == x.color) return true;
+    return false;
+}
+bool RedBlackBST::Node::isNull() {
+    if (this == NULL) return true;
+    return false;
+}
+bool RedBlackBST::isRed(Node *h) {
+    if (h->isNull()) return false;
+    return h->color == RED;
+}
+RedBlackBST::Node RedBlackBST::rotateLeft(RedBlackBST::Node h) {
+
+}
+RedBlackBST::Node RedBlackBST::rotateRight(RedBlackBST::Node h) {
+
+}
+void RedBlackBST::flipColors(RedBlackBST::Node h) {
+
+}
+int RedBlackBST::size() {
+
+}
+void RedBlackBST::put(double key, double val) {
+
+}
+RedBlackBST::Node RedBlackBST::put(RedBlackBST::Node h, double key, double val) {
+
+}
